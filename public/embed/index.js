c31c5d2f3ca805765b928a647f3918a8e4f09083
Chris Deighton <chris@serato.com>
2021-10-19 00:16:30 +1300

    UI niceness for embed page

--- public/embed/index.js
+++ public/embed/index.js
@@ -1,41 +1,65 @@
 const playerProductionDomain = 'https://player.serato.com/'
 
-let playerHtmlDoc = '/index.html'
-// let playerHtmlDoc = '/dist/player.html'
 let playerURL = ''
+const iframe = document.getElementsByTagName('iframe')[0]
 
 // Text boxes that define the embed dimensions
 Array.from(document.getElementsByClassName('embed-size')).forEach(ele => {
   ele.addEventListener('keyup', setEmbedSize)
   ele.addEventListener('change', setEmbedSize)
+  ele.addEventListener('focus', setFrameborder)
+  ele.addEventListener('blur', setFrameborder)
 })
+
 // Radio buttons that specify the video source
 document.getElementsByName('vid-src').forEach(ele => ele.addEventListener('click', setPlayerUrl))
+
 // Text boxes that provide video source
 Array.from(document.getElementsByClassName('vid-src-text')).forEach(ele => {
   ele.addEventListener('keyup', setPlayerUrl)
   ele.addEventListener('change', setPlayerUrl)
 })
 
+// Radio buttons that specify the HTML document
+document.getElementsByName('html-src').forEach(ele => ele.addEventListener('click', setPlayerUrl))
+
 function setEmbedSize() {
-  const iframes = document.getElementsByTagName('iframe')
-  iframes[0].width = document.getElementById('embed-width').value
-  iframes[0].height = document.getElementById('embed-height').value
+  document.getElementById('iframe-container').style.width = document.getElementById('embed-width').value + 'px'
+  document.getElementById('iframe-container').style.height = document.getElementById('embed-height').value + 'px'
+  iframe.width = document.getElementById('embed-width').value
+  iframe.height = document.getElementById('embed-height').value
+  setEmbedHtmlSnippet()
+  setFrameborder()
 }
 
 function setPlayerUrl() {
-  const params = getVidParams()
+  const params = getPlayerUrlParams()
   if (params.length > 0) {
-    console.log([playerURL, playerHtmlDoc + '?' + params.join('&')])
+    const playerHtmlDoc = getPlayerHtmlDoc()
     if (playerURL !== playerHtmlDoc + '?' + params.join('&')) {
       playerURL = playerHtmlDoc + '?' + params.join('&')
-      const iframes = document.getElementsByTagName('iframe')
-      iframes[0].src = playerURL
+      iframe.src = playerURL
+      document.getElementById('embed-url').innerHTML = playerURL
+      setEmbedHtmlSnippet()
     }
+  }
+  setFrameborder()
+}
+
+function setEmbedHtmlSnippet() {
+  if (playerURL !== '') {
+    document.getElementById('embed-html').innerHTML = htmlencode(iframe.outerHTML)
   }
 }
 
-function getVidParams() {
+function setFrameborder() {
+  let show = playerURL === '' ||
+              document.activeElement.id === 'embed-width' ||
+              document.activeElement.id === 'embed-height'
+  document.getElementById('iframe-container').style.borderColor = show ? 'rgb(245, 118, 196)' : '#000'
+}
+
+function getPlayerUrlParams() {
   const radios = document.getElementsByName('vid-src')
   const params = []
   Array.from(document.getElementsByClassName('vid-src-text')).forEach(ele => {
@@ -61,4 +85,19 @@ function getVidParams() {
     }
   }
   return params
+}
+
+function getPlayerHtmlDoc() {
+  const radios = document.getElementsByName('html-src')
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return radios[i].dataset.src
+    }
+  }
+}
+
+function htmlencode(html) {
+  const div = document.createElement('div')
+  div.appendChild(document.createTextNode(html))
+  return div.innerHTML
 }
